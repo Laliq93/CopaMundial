1e4b57cd44bc707d5f3ffd8648b063e6a06c807e
Unknown <edwarddenobrega@gmail.com>
2018-04-26 13:44:19 -0400

    actualizando repo local

--- COPA_MUNDIAL_UCAB-Develop/src/app/MODULO2/admin/consultar-ciudad/consultar-ciudad.component.ts
+++ COPA_MUNDIAL_UCAB-Develop/src/app/MODULO2/admin/consultar-ciudad/consultar-ciudad.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-consultar-ciudad',
   templateUrl: './consultar-ciudad.component.html',
   styleUrls: ['./consultar-ciudad.component.css']
 })
-export class ConsultarCiudadComponent implements OnInit {
+export class ConsultarCiudadComponent  {
 
-  constructor() { }
+  constructor(private router: Router){}
 
-  ngOnInit() {
+  volver(): void {
+		this.router.navigate(['admin/city']);
   }
-
+  
 }
