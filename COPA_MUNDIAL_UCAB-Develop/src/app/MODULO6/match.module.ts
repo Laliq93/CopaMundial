a4d998171e435cf8a3e98f31e39c41738e6b53f3
MinervaMorales <minervamoralesruiz@gmail.com>
2018-04-30 18:38:11 -0400

    Modificando el ts de match

--- COPA_MUNDIAL_UCAB-Develop/src/app/MODULO6/match.module.ts
+++ COPA_MUNDIAL_UCAB-Develop/src/app/MODULO6/match.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatchComponent } from './client/match.component';
 import { MatchAdminComponent } from './admin/match-admin.component';
+import { KnockoutPhaseComponent } from './client/knockout-phase/knockout-phase.component';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
   	MatchComponent,
-  	MatchAdminComponent
+  	MatchAdminComponent,
+  	KnockoutPhaseComponent
   ],
   providers: [],
   exports: [
