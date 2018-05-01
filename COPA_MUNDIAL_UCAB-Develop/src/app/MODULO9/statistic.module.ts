56876a827f779cd7d426f7fd54202906e42119d2
Yonder <yonderrosales96@gmail.com>
2018-05-01 17:09:05 -0400

    cambios

--- COPA_MUNDIAL_UCAB-Develop/src/app/MODULO9/statistic.module.ts
+++ COPA_MUNDIAL_UCAB-Develop/src/app/MODULO9/statistic.module.ts
@@ -4,7 +4,7 @@ import { StatisticComponent } from './client/statistic.component';
 import { StatisticAdminComponent } from './admin/statistic-admin.component';
 import { GroupComponent } from './group/group.component';
 import { DatepickerComponent } from './datepicker/datepicker.component';
-import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+//import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
 //import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @NgModule({
