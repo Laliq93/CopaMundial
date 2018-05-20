6df99138c2ddd41e34e4e585e5ef212b62640fac
Wilmer Perez <wjoseperez20@gmail.com>
2018-05-19 23:10:29 -0400

    Cambios + Error con el side
    Luis nos va a ayudar :)

---
+++ COPA_MUNDIAL_UCAB-Develop/src/app/home/contenido-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: "contenido-home",
+  templateUrl: "./contenido-home.component.html",
+  styleUrls: ["./home.component.css"]
+})
+export class RecoveryComponent {
+
+}
