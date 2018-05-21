2023080dd54b3955cc20a20b6a1aba4867b99b92
Wilmer Perez <wjoseperez20@gmail.com>
2018-05-20 23:58:18 -0400

    Vistas Con Get v2
    Vistas , controladores y modulos

--- COPA_MUNDIAL_UCAB-Develop/src/app/home/contenido-home.ts
+++ COPA_MUNDIAL_UCAB-Develop/src/app/home/contenido-home.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 /* tslint:disable */
 
 @Component({
-  selector: "contenido-home",
-  templateUrl: "./contenido-home.component.html",
-  styleUrls: ["./home.component.css"]
+  selector: 'contenido-home',
+  templateUrl: './contenido-home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class RecoveryComponent {
 
