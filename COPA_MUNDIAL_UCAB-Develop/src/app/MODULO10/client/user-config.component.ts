c6375b259bc8fdb08e91e5cc561bb7e8d8ee0351
Wilmer Perez <wjoseperez20@gmail.com>
2018-05-20 23:30:40 -0400

    Cambios Vistas
    DatePickers, Get, Set y views

--- COPA_MUNDIAL_UCAB-Develop/src/app/MODULO10/client/user-config.component.ts
+++ COPA_MUNDIAL_UCAB-Develop/src/app/MODULO10/client/user-config.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, AfterViewInit } from "@angular/core";
+/* tslint:disable */
+
+declare var $: any;
 
 @Component({
-  selector: 'user-config',
-  templateUrl: './user-config.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "user-config",
+  templateUrl: "./user-config.component.html",
+  styleUrls: ["./user.component.css"]
 })
-
 export class UserConfigComponent {
-public modulo: string = 'diez';
+  constructor(private _zone: NgZone) {
+
+  }
+ 
+  ngAfterViewInit(): void {
+    this._zone.runOutsideAngular(() => {
+      $("#exampleInputDatePicker1").pickdate();
+    });
+  }
 }
