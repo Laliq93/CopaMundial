b4231ecbf856396c4090fd9014b76612a77d9593
marcoinfanti <marcoinfanti1994@gmail.com>
2018-05-24 22:17:33 -0400

    Ahora si agrego las benditas vistas

---
+++ COPA-MUNDIAL-FRONT/src/app/modulo02/components/consultar-ciudad/consultar-ciudad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consultar-ciudad',
+  templateUrl: './consultar-ciudad.component.html',
+  styleUrls: ['./consultar-ciudad.component.css']
+})
+export class ConsultarCiudadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- COPA-MUNDIAL-FRONT/src/app/modulo02/modulo02.module.ts
+++ COPA-MUNDIAL-FRONT/src/app/modulo02/modulo02.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Modulo02RoutingModule } from './modulo02-routing.module';
-import { AdminComponent } from './components/admin/admin.component';
-import { ClientComponent } from './components/client/client.component';
-import { AgregarComponent } from './components/admin/agregar/agregar.component';
 import { AgregarCiudadComponent } from './components/agregar-ciudad/agregar-ciudad.component';
 import { EditarCiudadComponent } from './components/editar-ciudad/editar-ciudad.component';
 import { ConsultarCiudadComponent } from './components/consultar-ciudad/consultar-ciudad.component';
