9d58fd074142b727e8a16edf86b5a15da75f6c6c
Unknown <ismaelteixeira6@gmail.com>
2018-05-25 18:28:52 -0400

    Scripts BD Modulo 4

--- SCRIPTS/DDL.sql
+++ SCRIPTS/DDL.sql
@@ -8,6 +8,11 @@ CREATE DATABASE copamundial WITH OWNER = admin_copamundial ENCODING = UTF8;
 Drop table usuario;
 Drop SEQUENCE SEQ_Usuario;
 
+--Modulo 4
+--DROPS
+Drop table equipo;
+Drop SEQUENCE SEQ_Equipo
+
 --Creates Tables
 
 --Modulo 1
@@ -29,9 +34,37 @@ CREATE TABLE USUARIO (
 );
 --Fin de modulo
 
+--Modulo 4
+
+CREATE TABLE PAIS (
+
+	pa_id		integer,
+	pa_iso		varchar(3),
+	pa_nombre	varchar(20),
+
+    CONSTRAINT primaria_pais PRIMARY KEY(pa_id)
+);
+
+CREATE TABLE EQUIPO (
+
+	eq_id		integer,
+    eq_descripcion varchar(100) NOT NULL,
+    eq_status boolean default true NOT NULL,
+    eq_grupo varchar(1) CHECK (eq_grupo ='A' OR eq_grupo ='B' OR eq_grupo ='C' OR eq_grupo ='D' OR eq_grupo ='E' OR eq_grupo ='F' OR eq_grupo ='G' OR eq_grupo ='H'),
+    eq_pa_id  integer,
+
+    CONSTRAINT primaria_equipo PRIMARY KEY(eq_id),
+    CONSTRAINT eq_pa_id FOREIGN KEY (eq_pa_id) REFERENCES pais (pa_id)
+);
+--Fin de modulo 4
+
 --ALTERS
 --Modulo 1
 --Fin de modulo
+
+--ALTERS
+--Modulo 4
+--Fin de modulo 4
 
 --SEQUENCES
 --Modulo 1
@@ -43,11 +76,30 @@ CREATE SEQUENCE SEQ_Usuario
   CACHE 1;
 --Fin de modulo
 
+--Modulo 4
+CREATE SEQUENCE SEQ_Equipo
+  START WITH 1
+  INCREMENT BY 1
+  NO MINVALUE
+  NO MAXVALUE
+  CACHE 1;
+
+ CREATE SEQUENCE SEQ_Pais
+  START WITH 1
+  INCREMENT BY 1
+  NO MINVALUE
+  NO MAXVALUE
+  CACHE 1;
+--Fin de modulo 4
+
 --INDEX
 --Modulo 1
 --Fin de modulo
+--Modulo 4
+--Fin de modulo 4
 
 GRANT ALL PRIVILEGES ON TABLE usuario TO admin_copamundial;
+GRANT ALL PRIVILEGES ON TABLE equipo TO admin_copamundial;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO admin_copamundial;
 
 --Fin Creates tables
