8d80c7729372c30f9747c343e880e08771912196
marcoinfanti <marcoinfanti1994@gmail.com>
2018-05-25 08:48:29 -0400

    detalles

--- COPA-MUNDIAL-FRONT/src/app/modulo02/components/consultar-ciudad/consultar-ciudad.component.ts
+++ COPA-MUNDIAL-FRONT/src/app/modulo02/components/consultar-ciudad/consultar-ciudad.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./consultar-ciudad.component.css']
 })
 export class ConsultarCiudadComponent implements OnInit {
+  
+
+  nombreCiudades: string[] = ['Moscu', 'Ekaterimburgo','Leningrado ','San Petersburgo','Samara', 'Rostov','Kazan', 'Novorossiysk', 'Sochi', 'Saransk', 'Nizhni Nóvgorod']
+
 
   constructor() { }
 
