1f9c537b6d1dc705213b4b9c5f2b13c37fc3cac8
marcoinfanti <marcoinfanti1994@gmail.com>
2018-05-24 22:06:16 -0400

    agregando vistas de nuevo

--- COPA-MUNDIAL-FRONT/src/app/modulo02/modulo02.module.ts
+++ COPA-MUNDIAL-FRONT/src/app/modulo02/modulo02.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Modulo02RoutingModule } from './modulo02-routing.module';
+import { AdminComponent } from './components/admin/admin.component';
+import { ClientComponent } from './components/client/client.component';
+import { AgregarComponent } from './components/admin/agregar/agregar.component';
+import { AgregarCiudadComponent } from './components/agregar-ciudad/agregar-ciudad.component';
+import { EditarCiudadComponent } from './components/editar-ciudad/editar-ciudad.component';
+import { ConsultarCiudadComponent } from './components/consultar-ciudad/consultar-ciudad.component';
+import { DetalleCiudadComponent } from './components/detalle-ciudad/detalle-ciudad.component';
 
 @NgModule({
   imports: [
     CommonModule,
     Modulo02RoutingModule
   ],
-  declarations: []
+  declarations: [AgregarCiudadComponent, EditarCiudadComponent, ConsultarCiudadComponent, DetalleCiudadComponent]
 })
 export class Modulo02Module { }
