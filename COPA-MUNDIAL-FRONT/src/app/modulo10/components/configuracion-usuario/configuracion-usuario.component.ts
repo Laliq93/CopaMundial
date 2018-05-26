710661dad19c6de405028d979b8a500089f9b329
Wilmer Perez <wjoseperez20@gmail.com>
2018-05-26 13:52:57 -0400

    vistas

---
+++ COPA-MUNDIAL-FRONT/src/app/modulo10/components/configuracion-usuario/configuracion-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-configuracion-usuario',
+  templateUrl: './configuracion-usuario.component.html',
+  styleUrls: ['./configuracion-usuario.component.css', '../style-usuario.component.css']
+})
+export class ConfiguracionUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
