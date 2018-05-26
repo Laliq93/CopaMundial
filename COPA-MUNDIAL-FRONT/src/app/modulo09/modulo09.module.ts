a1fb22b8457a8faa490d27f98eaeb5f846415de5
Jose Angel Calcano <jose.calcano07@gmail.com>
2018-05-26 01:20:41 -0400

    Estadistica Jugador

--- COPA-MUNDIAL-FRONT/src/app/modulo09/modulo09.module.ts
+++ COPA-MUNDIAL-FRONT/src/app/modulo09/modulo09.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { Modulo09RoutingModule } from './modulo09-routing.module';
 import { EstgeneralComponent } from './estgeneral/estgeneral.component';
 import { EstpartidoComponent } from './estpartido/estpartido.component';
+import { EstjugadorComponent } from './estjugador/estjugador.component';
 
 @NgModule({
   imports: [
     CommonModule,
     Modulo09RoutingModule
   ],
-  declarations: [EstgeneralComponent, EstpartidoComponent]
+  declarations: [EstgeneralComponent, EstpartidoComponent, EstjugadorComponent]
 })
 export class Modulo09Module { }
