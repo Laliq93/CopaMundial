88b7f39ea05ba25c05a5df1c5d967c780aec8808
Yonder <yonderrosales96@gmail.com>
2018-05-25 22:33:43 -0400

    cambios en modulo estadistica y arreglando merge en modulo2

--- COPA-MUNDIAL-FRONT/src/app/modulo09/estgeneral/estgeneral.component.ts
+++ COPA-MUNDIAL-FRONT/src/app/modulo09/estgeneral/estgeneral.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'app-estgeneral',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./estgeneral.component.css']
 })
 export class EstgeneralComponent implements OnInit {
-
-  constructor() { }
+  grupoactual:number = 1;
+  partidos:boolean = false;
+  estadisticas:boolean = true;
+  numero:number;
+  constructor(private router: Router){}
 
   ngOnInit() {
+    $('#picker').pickdate();
   }
+
+  clickedgrupo(valor: number):void  {
+    // yourText is the argument from the template
+    if (valor > 0 && valor<9) {
+      this.grupoactual =valor;
+    }
+    console.log(this.grupoactual);
+    this.router.navigate(['estadisticas/estadisticas']);
+    
+  }
+
+  ClickPartidos(){
+    this.partidos = false;
+    this.estadisticas = true;
+    console.log("partidos");
+    this.router.navigate(['estadisticas/estadisticas']);
+  }
+  ClickEstadisticas(){
+    this.partidos = true;
+    this.estadisticas = false;
+
+    console.log("estadisticas");
+    this.router.navigate(['estadisticas/estadisticas']);
+  }
+
+  click(){
+    console.log("aqui el nombre: "+this.numero);
+  }
+
+
 
 }
