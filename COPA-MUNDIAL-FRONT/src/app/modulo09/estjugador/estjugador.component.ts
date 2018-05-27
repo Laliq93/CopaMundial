d1e340168ac2b839056f18e51d0416bbb954d57c
marialead <alejandrarriaga07@gmail.com>
2018-05-27 16:50:40 -0400

    pantalla estjugador dinamica

--- COPA-MUNDIAL-FRONT/src/app/modulo09/estjugador/estjugador.component.ts
+++ COPA-MUNDIAL-FRONT/src/app/modulo09/estjugador/estjugador.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstjugadorComponent implements OnInit {
 
+  datos:any = [
+    {pais: 'México', goles: 50, asistencias:5,tamarillas:5,trojas:4,mjugados:4,calificacion:5},
+    {pais: 'Polonia', goles: 50, asistencias:5,tamarillas:5,trojas:4,mjugados:4,calificacion:5},
+    {pais: 'Iran', goles: 50, asistencias:5,tamarillas:5,trojas:4,mjugados:4,calificacion:5}
+  ];   
+
+  jugador = {nombre:'Christiano Ronaldo',peso:60,altura:1.2,edad:38};
+
   constructor() { }
 
   ngOnInit() {
