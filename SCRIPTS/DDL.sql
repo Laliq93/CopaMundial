ee4634b8c18ebc1de79b2cbc8ef6e30939815223
Luis Valladares <luisvalladares94@gmail.com>
2018-05-27 12:51:42 -0400

    Fix DDL

--- SCRIPTS/DDL.sql
+++ SCRIPTS/DDL.sql
@@ -14,6 +14,8 @@ Drop table equipo;
 Drop SEQUENCE SEQ_Equipo;
 Drop table pais;
 Drop SEQUENCE SEQ_Pais;
+Drop table i18n_equipo;
+Drop SEQUENCE SEQ_i18n_Equipo;
 
 --Creates Tables
 
@@ -38,11 +40,19 @@ CREATE TABLE USUARIO (
 
 --Modulo 4
 
+CREATE TABLE I18N_EQUIPO (
+    i18n_id    integer NOT NULL,
+    i18n_idioma     varchar(100) NOT NULL,
+    i18n_mensaje    text NOT NULL,
+    UNIQUE (i18n_id),
+    CONSTRAINT primaria_i18n_equipo PRIMARY KEY (i18n_id, i18n_idioma)
+);
+
 CREATE TABLE PAIS (
 
 	pa_iso		varchar(3),
-	pa_i18n_nombre	varchar(20),
-    pa_urlBandera varchar(50),
+	pa_i18n_nombre	integer NOT NULL,
+	pa_urlBandera varchar(50),
 
     CONSTRAINT primaria_pais PRIMARY KEY(pa_iso),
     CONSTRAINT pa_i18n_nombre FOREIGN KEY (pa_i18n_nombre) REFERENCES i18n_equipo (i18n_id)
@@ -51,25 +61,16 @@ CREATE TABLE PAIS (
 CREATE TABLE EQUIPO (
 
 	eq_id		integer,
-    eq_i18n_descripcion integer NOT NULL,
-    eq_status boolean default true NOT NULL,
-    eq_grupo varchar(1) CHECK (eq_grupo ='A' OR eq_grupo ='B' OR eq_grupo ='C' OR eq_grupo ='D' OR eq_grupo ='E' OR eq_grupo ='F' OR eq_grupo ='G' OR eq_grupo ='H'),
-    eq_pa_id  varchar(3),
-    eq_habilitado boolean,
+	eq_i18n_descripcion integer NOT NULL,
+	eq_status boolean default true NOT NULL,
+	eq_grupo varchar(1) CHECK (eq_grupo ='A' OR eq_grupo ='B' OR eq_grupo ='C' OR eq_grupo ='D' OR eq_grupo ='E' OR eq_grupo ='F' OR eq_grupo ='G' OR eq_grupo ='H'),
+	eq_pa_id  varchar(3),
+	eq_habilitado boolean,
 
     CONSTRAINT primaria_equipo PRIMARY KEY(eq_id),
     CONSTRAINT eq_pa_id FOREIGN KEY (eq_pa_id) REFERENCES pais (pa_iso),
     CONSTRAINT eq_i18n_descripcion FOREIGN KEY (eq_i18n_descripcion) REFERENCES i18n_equipo (i18n_id)
 );
-
-CREATE TABLE I18N_EQUIPO (
-    i18n_id    integer NOT NULL,
-    i18n_idioma     varchar(100) NOT NULL,
-    i18n_mensaje    text NOT NULL,
-
-    CONSTRAINT primaria_i18n_equipo PRIMARY KEY(i18n_id, i18n_idioma),
-);
-
 --Fin de modulo 4
 
 --ALTERS
@@ -92,6 +93,13 @@ CREATE SEQUENCE SEQ_Usuario
 
 --Modulo 4
 CREATE SEQUENCE SEQ_Equipo
+  START WITH 1
+  INCREMENT BY 1
+  NO MINVALUE
+  NO MAXVALUE
+  CACHE 1;
+
+  CREATE SEQUENCE SEQ_i18n_Equipo
   START WITH 1
   INCREMENT BY 1
   NO MINVALUE
