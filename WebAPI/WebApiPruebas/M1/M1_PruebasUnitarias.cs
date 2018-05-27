83d7c1250535aabaf3a9e00852e15b7b4165629a
Unknown <lvqp.93@gmail.com>
2018-05-27 01:13:16 -0400

    Mas Pruebas Unitarias

--- WebAPI/WebApiPruebas/M1/M1_PruebasUnitarias.cs
+++ WebAPI/WebApiPruebas/M1/M1_PruebasUnitarias.cs
@@ -14,21 +14,33 @@ namespace WebApiPruebas.M1
    [TestFixture]
     public class M1_PruebasUnitarias
     {
-        M1_RegistroLoginRecuperarController _prueba;
+        Usuario _usuarioprueba;
+        M1_RegistroLoginRecuperarController _pruebacontroller;
 
        [SetUp]
         public void Init()
         {
-            _prueba = new M1_RegistroLoginRecuperarController();
+        _pruebacontroller = new M1_RegistroLoginRecuperarController();
+     /*   _usuarioprueba = new Usuario
+       {
+        _nombreUsuario = "alefernandez",
+        _nombre = "Alejandro"
+        _apellido = "Fernandez",
+        _fechaNacimiento = "14/09/1993",
+        _correo = "pedropaff7@gmail.com",
+        _genero = "M",
+        _esAdmin = 0;
+        _password "ale12345"
+      };
+      */
         }
-
         [Test]
         public void AgregarUsuarioTest()
         {
            //valida que el usuario que se esta ingresando ingresando, exista en la bd
-            _nombreusuario2 = "lauraquinones";
-            _prueba.AgregarUsuario("lauraquinones","Laura","Quinones","14/09/1993","lvqp.93@gmail.com","F","laliquinones14");
-            _prueba.ValidarNombreUsuario(_nombreusuario2);
+            string _nombreusuario2 = "lauraquinones";
+            _pruebacontroller.AgregarUsuario("lauraquinones","Laura","Quinones","14/09/1993","lvqp.93@gmail.com","F","laliquinones14");
+            _pruebacontroller.ValidarNombreUsuario(_nombreusuario2);
 
         }
 
@@ -37,23 +49,47 @@ namespace WebApiPruebas.M1
         {
             //valido que envia el correo para recuperar la contrasena
         }
-
+/* 
         [Test]
-        public void IniciarSesionTest()
+        public void IniciarSesionCorreoTest()
         {
             //valido si el usuario existe y ese es su password
- 
+            int _idprueba = 55;
+            int _id = _prueba.IniciarSesionCorreo("lvqp.93@gmail.com", "laliquinones14");
+            Assert.AreEqual(_idprueba,_id);
         }
 
         [Test]
-        public void ExcepcionUsuarioNoExisteTest()
+        public void IniciarSesionUsuarioTest()
         {
-            //valido que la excepcion que muestra cuando el usuario no existe sea correcta
+            //valido si el usuario existe y ese es su password
+            int _idprueba = 55;
+            int _id = _prueba.IniciarSesionUsuario("lauraquinones", "laliquinones14");
+            Assert.AreEqual(_idprueba,_id);
         }
+*/
+ [Test]
+        public void CorreoExistenteExceptionTest()
+        {
+            //intento insertar persona con iguales correos
+          _pruebacontroller.AgregarUsuario("lauraquinones","Laura","Quinones","14/09/1993","lvqp.93@gmail.com","F","laliquinones14");
+          _pruebacontroller.AgregarUsuario("probando123","Laura","Quinones","14/09/1993","lvqp.93@gmail.com","F","laliquinones14");
+        }
+ 
+        [Test]
+        public void NombreUsuarioExistenteExceptionTest()
+        {
+            //intento insertar personas con iguales nombres de usuario
+          _pruebacontroller.AgregarUsuario("lauraquinones","Laura","Quinones","14/09/1993","lvqp.93@gmail.com","F","laliquinones14");
+          _pruebacontroller.AgregarUsuario("lauraquinones","Laura","Quinones","14/09/1993","probando123@gmail.com","F","laliquinones14");
+        }
+        }
+ 
+
         [TearDown]
         public void End()
         {
-            _prueba = null;
+            _pruebacontroller = null;
            
         }
 
