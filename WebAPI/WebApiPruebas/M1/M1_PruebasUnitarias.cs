3b397702d53a2d8932031dff2b993718509c4f74
Unknown <lvqp.93@gmail.com>
2018-05-26 22:24:11 -0400

    PUAgregarUsuario

---
+++ WebAPI/WebApiPruebas/M1/M1_PruebasUnitarias.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WebAPI.Models.Excepciones;
+using WebAPI.Models;
+using WebAPI.Controllers;
+
+
+namespace WebApiPruebas.M1
+{
+   [TestFixture]
+    public class M1_PruebasUnitarias
+    {
+        M1_RegistroLoginRecuperarController _prueba;
+
+       [SetUp]
+        public void Init()
+        {
+            _prueba = new M1_RegistroLoginRecuperarController();
+        }
+
+        [Test]
+        public void AgregarUsuarioTest()
+        {
+           //valido que el usuario que estoy ingresando exista
+            _nombreusuario2 = "lauraquinones";
+            _prueba.AgregarUsuario("lauraquinones","Laura","Quinones","14/09/1993","lvqp.93@gmail.com","F","laliquinones14");
+            _prueba.ValidarNombreUsuario(_nombreusuario2);
+
+        }
+
+        [Test]
+        public void EnviarCorreoTest()
+        {
+            //valido que envia el correo para recuperar la contrasena
+        }
+
+        [Test]
+        public void IniciarSesionTest()
+        {
+            //valido si el usuario existe y ese es su password
+ 
+        }
+
+        [Test]
+        public void ExcepcionUsuarioNoExisteTest()
+        {
+            //valido que la excepcion que muestra cuando el usuario no existe sea correcta
+        }
+        [TearDown]
+        public void End()
+        {
+            _prueba = null;
+           
+        }
+
+    }
+}
