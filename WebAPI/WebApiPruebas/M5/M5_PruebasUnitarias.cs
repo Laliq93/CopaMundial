5bb7765724f4907736145d30fb1efcdc8af20754
daloyo95 <39608985+daloyo95@users.noreply.github.com>
2018-05-28 13:08:25 -0400

    .

--- WebAPI/WebApiPruebas/M5/M5_PruebasUnitarias.cs
+++ WebAPI/WebApiPruebas/M5/M5_PruebasUnitarias.cs
@@ -1,13 +1,8 @@
-﻿using NUnit.Framework;
-using WebAPI.Models.Excepciones;
-using WebAPI.Models;
-using WebAPI.Controllers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 
 namespace WebApiPruebas.M5
 {
