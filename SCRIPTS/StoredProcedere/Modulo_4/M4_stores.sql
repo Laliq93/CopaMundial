87eb032a4fb31fbf81c4ff5adb0b4ab1251e7355
unknown <marvianmontoya@hotmail.com>
2018-05-28 00:49:50 -0400

    SP modificar listo, acomodados otros SP

--- SCRIPTS/StoredProcedere/Modulo_4/M4_stores.sql
+++ SCRIPTS/StoredProcedere/Modulo_4/M4_stores.sql
@@ -1,18 +1,22 @@
 CREATE OR REPLACE FUNCTION m4_traer_pais(idioma VARCHAR(2))
-RETURNS TABLE (iso VARCHAR(3), nombre text)
+RETURNS TABLE (iso VARCHAR(3), nombre text, pk, id)
 
 AS $$
 DECLARE
    var_r    record;
 BEGIN
-	FOR var_r IN(SELECT pa_iso, pa_i18n_nombre, i18n_mensaje
+	FOR var_r IN(SELECT pa_iso, pa_i18n_nombre, i18n_mensaje, i18n_pk, i18n_id
 		FROM PAIS, I18N_EQUIPO
 		WHERE pa_i18n_nombre = i18n_id and i18n_idioma = idioma)
 
 	LOOP
 
-	iso = pa_iso;
-	nombre = i18n_idioma;
+	iso = var_r.pa_iso;
+	nombre = var_r.i18n_idioma;
+	pk = var_r.i18n_pk;
+	id = var_r.i18n_id
+	
+
 
 	RETURN NEXT;
    END LOOP;
@@ -42,36 +46,66 @@ BEGIN
 
 END;
 $$ LANGUAGE plpgsql;
-*/
+
 
 
 
   CREATE OR REPLACE FUNCTION m4_busca_equipo_iso
 (id_pais VARCHAR(3), idioma VARCHAR(2)) 
-RETURNS RETURNS TABLE(iso, _nombre, descripcion, grupo, status)
+RETURNS RETURNS TABLE(iso, _nombre, descripcion, grupo, status, id, pk )
 
 AS$$
+DECLARE
+   var_r;
 BEGIN
 	
-	Select pa_iso, (select i18n_mensaje from pais, i18n_equipo
+	FOR var_r IN ( Select pa_iso, (select i18n_mensaje from pais, i18n_equipo
 					where i18n_idioma = idioma and i18n_id = pa_i18n_nombre 
 					and pa_iso = id_pais) as nombre, 
-	i18n_mensaje as Descripcion , eq_grupo, eq_status
+	i18n_mensaje as Descripcion , eq_grupo, eq_status, i18n_id, i18n_pk
 	from equipo, pais, i18n_equipo
 	where i18n_idioma = idioma and i18n_id = eq_i18n_descripcion and pa_iso = id_pais 
-	and pa_iso = eq_pa_id
+	and pa_iso = eq_pa_id)
 
 	LOOP
 
-	iso = pa_iso;
-	_nombre = nombre;
-	descripcion = Descripcion;
-	grupo = eq_grupo;
-	status = eq_status
+	iso = var_r.pa_iso;
+	_nombre = var_r.nombre;
+	descripcion = var_r.Descripcion;
+	grupo = var_r.eq_grupo;
+	status = var_r.eq_status
+	id = var_r.i18n_id;
+	pk = var_r.i18n_pk;
 
 	RETURN NEXT;
    END LOOP;
 
 END;
 $$ LANGUAGE plpgsql;
-*/
+
+
+
+
+CREATE OR REPLACE FUNCTION m4_modificar_equipo
+( descripcionES VARCHAR(100), descripcionEN VARCHAR(100), 
+ grupo VARCHAR(1), id_pais VARCHAR(3), status boolean, grupo VARCHAR(1),
+ habilitado boolean) 
+RETURNS void AS
+$$
+BEGIN
+	
+	UPDATE I18N_EQUIPO SET (i18n_mensaje = descripcionEN)
+    WHERE (eq_i18n_descripcion = i18n_id and pa_iso = eq_pa_id and eq_pa_id = id_pais
+    and i18n_idioma = 'en');
+
+    UPDATE I18N_EQUIPO SET (i18n_mensaje = descripcionES)
+    WHERE (eq_i18n_descripcion = i18n_id and pa_iso = eq_pa_id and eq_pa_id = id_pais
+    and i18n_idioma = 'es');
+
+    UPDATE equipo SET (eq_grupo = grupo and eq_status = status and eq_grupo = grupo 
+    and eq_habilitado = habilitado )
+    WHERE (pa_iso = eq_pa_id and eq_pa_id = id_pais);
+
+
+END;
+$$ LANGUAGE plpgsql;
