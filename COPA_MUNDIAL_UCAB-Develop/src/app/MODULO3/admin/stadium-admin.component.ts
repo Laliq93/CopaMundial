2e537cfb5a370486c8ac5c7e4023063a6708eea2
Wilmer Perez <wjoseperez20@gmail.com>
2018-05-28 20:03:54 -0400

    Solucionando Felix

--- COPA_MUNDIAL_UCAB-Develop/src/app/MODULO3/admin/stadium-admin.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'stadium-admin',
-  templateUrl: './stadium-admin.component.html',
-  styleUrls: ['./stadium-admin.component.css']
-})
-export class StadiumAdminComponent {}
--- COPA_MUNDIAL_UCAB-Develop/src/assets/js/dateToday.js
+++
@@ -1,4 +0,0 @@
-var headline = angular.module('headline', []);
-headline.controller('datCtrl', function($scope) {
-    $scope.today = new Date();
-});
