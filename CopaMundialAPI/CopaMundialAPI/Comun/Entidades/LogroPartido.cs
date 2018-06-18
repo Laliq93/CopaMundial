18280b328d3a810e574add29e5c608ca12be711f
FelixMorales <felix_morales_@hotmail.com>
2018-06-17 20:38:27 -0400

    Quitado equipos, agregando app.config

--- CopaMundialAPI/CopaMundialAPI/Comun/Entidades/LogroPartido.cs
+++ CopaMundialAPI/CopaMundialAPI/Comun/Entidades/LogroPartido.cs
@@ -7,5 +7,10 @@ namespace CopaMundialAPI.Comun.Entidades
 {
     public class LogroPartido : Entidad
     {
+        //private Partido _partido;
+        private string _logro;
+        
+        //public Partido Partido { get => _partido; set => _partido = value; }
+        public string Logro { get => _logro; set => _logro = value; }
     }
 }
