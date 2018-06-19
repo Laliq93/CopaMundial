8b622610ea45f40c81f665b988f125171bcd267e
FelixMorales <felix_morales_@hotmail.com>
2018-06-19 19:40:03 -0400

    Agregando nuevos DTO y Traductores

---
+++ CopaMundialAPI/CopaMundialAPI/Servicios/DTO/Apuestas/DTOApuestaCantidad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CopaMundialAPI.Servicios.DTO.Apuestas
+{
+    public class DTOApuestaCantidad
+    {
+        private int _idUsuario;
+        private int _idLogro;
+        private int _apuestaUsuario;
+        private string _estado;
+
+        public int IdUsuario { get => _idUsuario; set => _idUsuario = value; }
+        public int IdLogro { get => _idLogro; set => _idLogro = value; }
+        public int ApuestaUsuario { get => _apuestaUsuario; set => _apuestaUsuario = value; }
+        public string Estado { get => _estado; set => _estado = value; }
+    }
+}
