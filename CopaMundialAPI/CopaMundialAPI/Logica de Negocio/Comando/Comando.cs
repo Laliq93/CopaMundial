1e6b55c75fe25f2163eca078e371d3a4dde08d7b
José Ángel Calcaño Azócar <jose.calcano07@gmail.com>
2018-06-21 22:19:23 -0400

    Modificando Comando y agregando DTO, traductor Ciudad

--- CopaMundialAPI/CopaMundialAPI/Logica de Negocio/Comando/Comando.cs
+++ CopaMundialAPI/CopaMundialAPI/Logica de Negocio/Comando/Comando.cs
@@ -6,8 +6,15 @@ using CopaMundialAPI.Comun.Entidades;
 
 namespace CopaMundialAPI.Logica_de_Negocio.Comando
 {
+    /// <summary>
+    /// Superclase Comando
+    /// </summary>
     public abstract class Comando
     {
+        private Entidad entidad;
+
+        public Entidad Entidad { get => entidad; set => entidad = value; }
+
         public abstract void Ejecutar ( );
 
         public abstract Entidad GetEntidad();
