10ef7fd55c8954cf5f2c01d4e8eb30e5a9076f24
FelixMorales <felix_morales_@hotmail.com>
2018-06-23 20:08:58 -0400

    Agregando script DDL_M8

--- CopaMundialAPI/CopaMundialAPI/Servicios/Fabrica/FabricaDTO.cs
+++ CopaMundialAPI/CopaMundialAPI/Servicios/Fabrica/FabricaDTO.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using CopaMundialAPI.Servicios.DTO.Apuestas;
 using CopaMundialAPI.Servicios.DTO.Ciudades;
-
+using CopaMundialAPI.Servicios.DTO.Partidos;
 
 namespace CopaMundialAPI.Servicios.Fabrica
 {
@@ -28,6 +28,11 @@ namespace CopaMundialAPI.Servicios.Fabrica
         public static DTOCiudad CrearDTOCiudad (string nombre, int habitantes, string descripcion, string nombreIngles, string descripcionIngles)
         {
             return new DTOCiudad (nombre,habitantes,descripcion,nombreIngles,descripcionIngles);
+        }
+
+        public static DTOListarProximosPartidos CrearDTOListarProximosPartidos()
+        {
+            return new DTOListarProximosPartidos();
         }
     }
 }
