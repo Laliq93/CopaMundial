4d4d7331f753cd75a8b0af444c6cc819e6e1bf61
Luis Valladares <luisvalladares94@gmail.com>
2018-06-25 21:40:03 -0400

    Entidades y DTO base

---
+++ CopaMundialAPI/CopaMundialAPI/Comun/Entidades/Alineacion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CopaMundialAPI.Comun.Entidades
+{
+    /// <summary>
+    /// Almacena las alineaciones asociadas a los partidos.
+    /// </summary>
+    public class Alineacion: Entidad
+    {
+        // al_capitan
+        private Boolean _esCapitan;
+        // al_posicion
+        private String _posicion;
+        // al_titular
+        private Boolean _esTitular;
+
+        // al_ju_id
+        private Jugador _jugador;
+        // al_eq_id
+        private Int32 _equipo;
+        // al_pa_id
+        private Partido _partido;
+
+        public bool EsCapitan { get => _esCapitan; set => _esCapitan = value; }
+        public string Posicion { get => _posicion; set => _posicion = value; }
+        public bool EsTitular { get => _esTitular; set => _esTitular = value; }
+
+        public Jugador Jugador { get => _jugador; set => _jugador = value; }
+        public int Equipo { get => _equipo; set => _equipo = value; }
+        public Partido Partido { get => _partido; set => _partido = value; }
+    }
+}
--- CopaMundialAPI/CopaMundialAPI/Comun/Entidades/Partido.cs
+++ CopaMundialAPI/CopaMundialAPI/Comun/Entidades/Partido.cs
@@ -5,14 +5,38 @@ using System.Web;
 
 namespace CopaMundialAPI.Comun.Entidades
 {
+    /// <summary>
+    /// Almacena la información asociada a los partidos jugados o por jugar.
+    /// </summary>
     public class Partido : Entidad
     {
-        private Equipo _equipo1;
-        private Equipo _equipo2;
-        private DateTime _fechaPartido;
+        // pa_fechaInicio
+        private DateTime _fechaInicioPartido;
+        // pa_fechaFin
+        private DateTime _fechaFinPartido;
+        // pa_arbitro
+        private String _arbitro;
+        // pa_status
+        private Boolean _estatus;
 
-        public Equipo Equipo1 { get => _equipo1; set => _equipo1 = value; }
-        public Equipo Equipo2 { get => _equipo2; set => _equipo2 = value; }
-        public DateTime FechaPartido { get => _fechaPartido; set => _fechaPartido = value; }
+        // pa_eq1_id
+        private Int32 _equipo1;
+        // pa_eq2_id
+        private Int32 _equipo2;
+        // pa_es_id
+        private Int32 _estadio;
+
+        private List<Alineacion> _alineaciones;
+
+        public DateTime FechaInicioPartido { get => _fechaInicioPartido; set => _fechaInicioPartido = value; }
+        public DateTime FechaFinPartido { get => _fechaFinPartido; set => _fechaFinPartido = value; }
+        public string Arbitro { get => _arbitro; set => _arbitro = value; }
+        public bool Estatus { get => _estatus; set => _estatus = value; }
+
+        public Int32 Equipo1 { get => _equipo1; set => _equipo1 = value; }
+        public Int32 Equipo2 { get => _equipo2; set => _equipo2 = value; }
+        public int Estadio { get => _estadio; set => _estadio = value; }
+
+        public List<Alineacion> Alineaciones { get => _alineaciones; set => _alineaciones = value; }
     }
 }
