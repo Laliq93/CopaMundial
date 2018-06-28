c3ee256073eb1c5c43737f958d2418bf077a708a
yonder <yonderrosales96@gmail.com>
2018-06-28 16:52:09 -0400

    mas pruebas unitarias

--- CopaMundialAPI/PruebasCopaMundialAPI/Modulo 2/PUCiudad.cs
+++ CopaMundialAPI/PruebasCopaMundialAPI/Modulo 2/PUCiudad.cs
@@ -1,5 +1,6 @@
 ﻿using CopaMundialAPI.Comun.Entidades;
 using CopaMundialAPI.Comun.Entidades.Fabrica;
+using CopaMundialAPI.Comun.Excepciones;
 using CopaMundialAPI.Fuente_de_Datos.DAO;
 using CopaMundialAPI.Fuente_de_Datos.Fabrica;
 using CopaMundialAPI.Presentacion.Controllers;
@@ -18,7 +19,7 @@ namespace PruebasCopaMundialAPI
 	[TestFixture]
 	public class PUCiudad
     {
-		private CiudadController controller;
+		
 		private Ciudad ciudad;
 		private DTOCiudad dto;
 		private DAOCiudad dao;
@@ -27,7 +28,7 @@ namespace PruebasCopaMundialAPI
 		[SetUp]
 		public void SetUp()
 		{
-			controller = new CiudadController();
+			
 			dto = FabricaDTO.CrearDTOCiudad(1, "yonder", 10, "a", "a", "b");
 			dao = FabricaDAO.CrearDAOCiudad();
 			ciudad = FabricaEntidades.CrearCiudad("prueba", 5, "prueba", "prueba", "prueba");
@@ -36,11 +37,9 @@ namespace PruebasCopaMundialAPI
 		[Test]
 		public void AgregarCiudad()
 		{
-			//controller.InsertarCiudad(dto);
+			
 			dao.Agregar(ciudad);
-			Console.WriteLine(ciudad);
-			Ciudad ciudadobtenida = (Ciudad)dao.ObtenerTodos().Last();
-				Console.WriteLine(dao.ObtenerTodos().Last());
+			Ciudad ciudadobtenida = (Ciudad)dao.ObtenerTodos().Last();	
 			ciudadobtenida.Id = 0;
 			ciudad.Id = 0;
 
@@ -49,52 +48,44 @@ namespace PruebasCopaMundialAPI
 
 			Assert.AreEqual(ciudad.Nombre, ciudadobtenida.Nombre);
 			
-			//Assert.AreSame(ciudadobtenida, ciudad);
 		}
 
 
 		[Test]
 		public void ObtenerCiudad()
 		{
-			//dao.Agregar(ciudad);
-			//controller.InsertarCiudad(dto);
-				dao.Agregar(ciudad);
+			
+			dao.Agregar(ciudad);
 			Ciudad ciudadobtenida = (Ciudad)dao.ObtenerTodos().Last();
 			Console.WriteLine(dao.ObtenerTodos().Last());
 
 			Assert.IsNotNull(ciudadobtenida);
-			//Assert.AreSame(ciudadobtenida, ciudad);
+			
 		}
 
 
 		[Test]
 		public void ObtenerCiudadPorNombre()
 		{
-			//dao.Agregar(ciudad);
-			//controller.InsertarCiudad(dto);
 			dao.Agregar(ciudad);
 			Ciudad ciudadobtenida = (Ciudad)dao.ConsultarCiudadPorNombre(ciudad).Last();
 			Console.WriteLine(dao.ObtenerTodos().Last());
 			
 			Assert.IsNotNull(ciudadobtenida);
-			//Assert.AreSame(ciudadobtenida, ciudad);
+			
 		}
 
 		[Test]
 		public void ObtenerCiudadPorId()
 		{
-			//dao.Agregar(ciudad);
-			//controller.InsertarCiudad(dto);
-			//Ciudad ciudad = FabricaEntidades.CrearCiudad(24, "prueba", 5, "prueba", "prueba", "prueba");
 			Ciudad ciudadobtenida = (Ciudad)dao.ObtenerTodos().Last();
 
 			Ciudad ciudad = new Ciudad(ciudadobtenida.Id);
-			//dao.Agregar(ciudad);
 			Ciudad city = (Ciudad)dao.ConsultarCiudadPorId(ciudad);
-			Console.WriteLine(dao.ObtenerTodos().Last());
+			
 
 			Assert.IsNotNull(city);
-			//Assert.AreSame(ciudadobtenida, ciudad);
+			
 		}
 
 		[Test]
@@ -116,10 +107,178 @@ namespace PruebasCopaMundialAPI
 			//Assert.AreSame(ciudadobtenida, ciudad);
 		}
 
+		[Test]
+		public void TestAgregarCiudadException()
+		{
+			
+			Assert.Catch<ObjetoNullException>(AgregarCiudadException);
+		}
+
+		
+		public void AgregarCiudadException()
+		{
+			Ciudad city = null;
+			dao.Agregar(city);
+			
+		}
+
+		[Test]
+		public void TestAgregarCiudadCastException()
+		{
+			
+			Assert.Catch<CasteoNoCorrectoException>(AgregarCiudadCastException);
+		}
+
+
+		public void AgregarCiudadCastException()
+		{
+			Usuario jugador = new Usuario();
+			dao.Agregar(jugador);
+
+		}
+
+
+		[Test]
+		public void TestEliminarCiudadException()
+		{
+
+			Assert.Catch<ObjetoNullException>(AgregarCiudadException);
+		}
+
+
+		public void EliminarCiudadException()
+		{
+			Ciudad city = null;
+			dao.Eliminar(city);
+
+		}
+
+		[Test]
+		public void TestConsultarCiudadException()
+		{
+
+			Assert.Catch<ObjetoNullException>(AgregarCiudadException);
+		}
+
+
+		public void ConsultarCiudadException()
+		{
+			Ciudad city = null;
+			dao.ConsultarCiudadPorId(city);
+
+		}
+
+		[Test]
+		public void TestConsultarCiudadPorNombreException()
+		{
+
+			Assert.Catch<ObjetoNullException>(AgregarCiudadException);
+		}
+
+
+		public void ConsultarCiudadPorNombreException()
+		{
+			Ciudad city = null;
+			dao.ConsultarCiudadPorNombre(city);
+
+		}
+
+		[Test]
+		public void TestConsultarCiudadPorNombreInglesException()
+		{
+
+			Assert.Catch<ObjetoNullException>(AgregarCiudadException);
+		}
+
+
+		public void ConsultarCiudadPorNombreInglesException()
+		{
+			Ciudad city = null;
+			dao.ConsultarCiudadPorNombreIngles(city);
+
+		}
+
+		[Test]
+		public void TestEliminarCiudadCastException()
+		{
+
+			Assert.Catch<CasteoNoCorrectoException>(EliminarCiudadCastException);
+		}
+
+
+		public void EliminarCiudadCastException()
+		{
+			Usuario jugador = new Usuario();
+			dao.Eliminar(jugador);
+
+		}
+
+		[Test]
+		public void TestConsultarCiudadCastException()
+		{
+
+			Assert.Catch<CasteoNoCorrectoException>(ConsultarCiudadCastException);
+		}
+
+
+		public void ConsultarCiudadCastException()
+		{
+			Usuario jugador = new Usuario();
+			dao.ConsultarCiudadPorId(jugador);
+
+		}
+
+		[Test]
+		public void TestConsultarCiudadPorNombreCastException()
+		{
+
+			Assert.Catch<CasteoNoCorrectoException>(ConsultarCiudadPorNombreCastException);
+		}
+
+
+		public void ConsultarCiudadPorNombreCastException()
+		{
+			Usuario jugador = new Usuario();
+			dao.ConsultarCiudadPorNombre(jugador);
+
+		}
+
+		[Test]
+		public void TestConsultarCiudadPorNombreInglesCastException()
+		{
+
+			Assert.Catch<CasteoNoCorrectoException>(ConsultarCiudadPorNombreInglesCastException);
+		}
+
+
+		public void ConsultarCiudadPorNombreInglesCastException()
+		{
+			Usuario jugador = new Usuario();
+			dao.ConsultarCiudadPorNombreIngles(jugador);
+
+		}
+		[Test]
+		public void TestModificarCiudad()
+		{
+			dao.Agregar(ciudad);
+			Ciudad ciudadobtenida = (Ciudad)dao.ObtenerTodos().Last();
+			Ciudad ciudadamodificar = new Ciudad(ciudadobtenida.Id,"nombremodificado",5,"descripcionmodificada","NombreEnModificado","DescripcionEnModificado");
+			dao.Actualizar(ciudadamodificar);
+			Ciudad ciudad1 = (Ciudad)dao.ObtenerTodos().Last();
+			
+			
+			Assert.AreEqual(ciudad1.Nombre, ciudadamodificar.Nombre);
+		}
+
+
+
+
+
+
 		[TearDown]
 		public void TeadDown()
 		{
-			controller = null;
+			
 			dto = null;
 			dao = null;
 		}
