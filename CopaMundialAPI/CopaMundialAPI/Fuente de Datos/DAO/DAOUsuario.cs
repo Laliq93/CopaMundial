1ec89fab3cb2dba82bd32c5a03182ae6d2f328a4
Unknown <lvqp.93@gmail.com>
2018-06-30 00:38:16 -0400

    DAO Usuario

---
+++ CopaMundialAPI/CopaMundialAPI/Fuente de Datos/DAO/DAOUsuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CopaMundialAPI.Comun.Entidades;
+using CopaMundialAPI.Fuente_de_Datos.DAO.Interfaces;
+
+namespace CopaMundialAPI.Fuente_de_Datos.DAO
+{
+    public class DAOUsuario : DAO, IDAOUsuario
+    {
+        public void Actualizar(Entidad entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Agregar(Entidad entidad)
+        {
+            throw new NotImplementedException();
+        }
+         
+        public void Eliminar(Entidad entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Entidad> ObtenerTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
