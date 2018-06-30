a10ec98abb1502cbadb681bcc6a93262eaeb4a34
saby112 <sabinaqg@gmail.com>
2018-06-29 20:09:16 -0400

    agregar jugador

--- COPA-MUNDIAL-FRONT/src/app/modulo05/components/jugadores/jugadores.component.ts
+++ COPA-MUNDIAL-FRONT/src/app/modulo05/components/jugadores/jugadores.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Jugador } from '../../models/jugador';
 import { Conexion } from '../../models/conexion';
 import { JugadorService } from '../../services/jugador.service';
 import {DTOMostrarJugador} from '../../models/dtomostrar-jugador';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {DTOActivarJugador} from '../../models/dtoactivar-jugador';
+import {HttpClient, HttpParams, HttpHeaders} from '@angular/common/http';
+import { FormControl, FormBuilder, Validators, NgForm } from '@angular/forms';
 import { Observable, Subject } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
+
 
 @Component({
   selector: 'app-jugadores',
   templateUrl: './jugadores.component.html',
   styleUrls: ['./jugadores.component.css']
 })
+
+@Injectable()
 export class JugadoresComponent implements OnInit {
   title = 'Jugadores';
   conexion : Conexion;
@@ -18,12 +30,17 @@ export class JugadoresComponent implements OnInit {
   ListJugadores: DTOMostrarJugador[] = [];
   ListJugadoresActivos: DTOMostrarJugador[] = [];
   ListJugadoresInactivos: DTOMostrarJugador[] = [];
+  jugador : Jugador;
+  activar_jugador : DTOActivarJugador;
+  desactivar_jugador : DTOActivarJugador;
 
+  readonly rootUrl =  'http://localhost:51543/api';
 
-  constructor(private jugadorService: JugadorService,
+  constructor(private router: Router,
     public http: HttpClient) 
     { 
       this.conexion = new Conexion();
+      this.jugador = new Jugador();
     }
 
   ngOnInit() {
@@ -33,11 +50,48 @@ export class JugadoresComponent implements OnInit {
     this.obtenerJugadoresInactivos();
 
   }
+
+  registrarJugador(playerRegistrationForm){
+    this.conexion.controller = 'agregarJugador';
+    this.url = this.conexion.GetApiJugador() + this.conexion.controller;
+
+    const httpHeaders = new HttpHeaders().set('Accept', 'application/json');
+
+
+    const {Nombre, Apellido, FechaNacimiento, 
+      LugarNacimiento, Peso, Altura, Posicion, 
+      Numero, Equipo} = playerRegistrationForm.controls;
+    
+
+    console.log(playerRegistrationForm.controls, Nombre.value, Apellido.value);
+    
+    
+    const player = {
+      nombre          : Nombre.value,
+      apellido        : Apellido.value,
+      fechaNacimiento : FechaNacimiento.value,
+      lugarNacimiento : LugarNacimiento.value,
+      peso            : Peso.value,
+      altura          : Altura.value,
+      posicion        : Posicion.value,
+      numero          : Numero.value,
+      equipo          : Equipo.value  
+    };
+
+      this.http
+      .post<Jugador>(this.url, player, httpOptions)
+      .subscribe(
+        success => {
+          console.log(success)
+        },
+        error => alert("Error en el sistema")
+      );
+    
+  }
   
   obtenerJugadores(){
     this.conexion.controller = 'obtenerJugadores';
     this.url = this.conexion.GetApiJugador() + this.conexion.controller;
-    console.log(this.http.get<DTOMostrarJugador>(this.url,  { responseType: 'json' }));
     
     this.http.get<DTOMostrarJugador>(this.url,  { responseType: 'json' }).subscribe(data => {
       for (let i = 0; i < Object.keys(data).length; i++) {
@@ -49,7 +103,6 @@ export class JugadoresComponent implements OnInit {
   obtenerJugadoresActivos(){
     this.conexion.controller = 'obtenerJugadoresActivo';
     this.url = this.conexion.GetApiJugador() + this.conexion.controller;
-    console.log(this.http.get<DTOMostrarJugador>(this.url,  { responseType: 'json' }));
     
     this.http.get<DTOMostrarJugador>(this.url,  { responseType: 'json' }).subscribe(data => {
       for (let i = 0; i < Object.keys(data).length; i++) {
@@ -62,7 +115,6 @@ export class JugadoresComponent implements OnInit {
   obtenerJugadoresInactivos(){
     this.conexion.controller = 'obtenerJugadoresInactivo';
     this.url = this.conexion.GetApiJugador() + this.conexion.controller;
-    console.log(this.http.get<DTOMostrarJugador>(this.url,  { responseType: 'json' }));
     
     this.http.get<DTOMostrarJugador>(this.url,  { responseType: 'json' }).subscribe(data => {
       for (let i = 0; i < Object.keys(data).length; i++) {
--- COPA-MUNDIAL-FRONT/src/app/modulo05/modulo05.module.ts
+++ COPA-MUNDIAL-FRONT/src/app/modulo05/modulo05.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-// extra imports from angular
 import { FormsModule } from '@angular/forms';
 import { DatePipe } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-
 import { Modulo05RoutingModule } from './modulo05-routing.module';
-
 import { JugadoresComponent } from './components/jugadores/jugadores.component';
 import { JugadorDetalleComponent } from './components/jugador-detalle/jugador-detalle.component';
-
 import { JugadorService } from './services/jugador.service';
+import { Routes, RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
