df1639a74c8f3985180c6c016b502e8835d15614
FelixMorales <felix_morales_@hotmail.com>
2018-07-02 02:21:58 -0400

    Arreglando script de crear base de datos y dos error response mas que faltaron por cambiar.

--- SCRIPTS/Ultima Entrega/Create DB copamundial.sql
+++ SCRIPTS/Ultima Entrega/Create DB copamundial.sql
@@ -2,7 +2,5 @@ CREATE DATABASE copamundial
     WITH 
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'Spanish_Bolivarian Republic of Venezuela.1252'
-    LC_CTYPE = 'Spanish_Bolivarian Republic of Venezuela.1252'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
